47d3c971b20143eafe22dfd1d564000741236751
Aditya yadav <ay2066694@gmail.com>
2026-09-12 14:25:46 +0530

    Fix currentUser handling

--- app.js
+++ app.js
@@ -83,12 +83,12 @@ passport.deserializeUser(User.deserializeUser());
 
 
 
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
-    res.locals.currentUser = req.user;
+    res.locals.currentUser = req.user || null;
     next();
-})
+});
 
 
 app.use("/listings", listingRouter);
