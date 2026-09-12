fac46679f9b4b5ccf97647a8a157361bdc08f6f6
Aditya yadav <ay2066694@gmail.com>
2026-09-12 15:58:51 +0530

    Test listing route

--- route/listing.js
+++ route/listing.js
@@ -12,7 +12,7 @@ const upload = multer({storage});
 const { indexRoute, newRoute, showRoute, createRoute, editRoute, updateRoute, deleteRoute } = require("../controllers/listing.js");
 
 router.route("/")
-.get(wrapAsync(indexRoute))
+.get(indexRoute)
 .post(isLoggedIn, upload.single("listing[image]"), validateListing, wrapAsync(createRoute));
 
 
